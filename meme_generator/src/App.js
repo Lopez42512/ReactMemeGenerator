61fa893fe9aecd7a90197f34ede559fe932aa5ec
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2019-12-13 21:01:57 -0500

    set up fetch request to meme api

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -2,10 +2,32 @@ import React, {Component} from "react"
 
 class App extends Component {
 
+    constructor(){
+        super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            imgUrl: "https://i.imgflip.com/345v97.jpg",
+            memeHolder: []
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.data.memes[0].url)
+            const {memes} = response.data
+            this.setState({
+                memeHolder: memes
+            })
+        })
+    }
+
     render(){
         return(
             <div>
-                hello world
+                <img src={this.state.imgUrl} />
             </div>
         )
     }
