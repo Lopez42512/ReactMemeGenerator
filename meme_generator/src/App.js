c8dc7b0907829aa7ec4a2cd698faa1314e0d5e60
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2019-12-14 20:35:13 -0500

    random memes is now being generated

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
             imgUrl: "https://i.imgflip.com/345v97.jpg",
             memeHolder: []
         }
+        this.eventHandle = this.eventHandle.bind(this)
+        this.submitHandle = this.submitHandle.bind(this)
     }
 
     componentDidMount(){
@@ -24,10 +26,48 @@ class App extends Component {
         })
     }
 
+    eventHandle(event){
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    submitHandle(event){
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.memeHolder.length)
+        const randMeme = this.state.memeHolder[randNum].url
+        this.setState({
+            imgUrl: randMeme
+        })
+
+    }
+
     render(){
         return(
             <div>
-                <img src={this.state.imgUrl} />
+                <form onSubmit={this.submitHandle}>
+                    <input 
+                        type="text"
+                        placeholder="Top Text"
+                        value={this.state.topText}
+                        onChange={this.eventHandle}
+                        name="topText"
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Bottom Text"
+                        value={this.state.bottomText}
+                        onChange={this.eventHandle}
+                        name="bottomText"
+                    />
+                    <button>Random Meme</button>
+                </form>
+                <div>
+                    <img src={this.state.imgUrl} alt="problem" />
+                    <h2>{this.state.topText}</h2>
+                    <h2>{this.state.bottomText}</h2>
+                </div>   
             </div>
         )
     }
