cb3a0758811be9895a4ee4a4b9ef35d26d7bb5d3
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-02-04 01:02:18 -0500

    user can now choose text font size

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,9 @@ import React from "react"
 
 function Header() {
     return (
-        <header>
+        <header className="header">
             <img 
+                className="headPic"
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
                 alt="Problem?"
             />
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,109 +1,66 @@
 import React, {Component} from "react"
-import Draggable from 'react-draggable';
+// import Draggable from 'react-draggable';
 
 class MemeGenerator extends Component {
-
     constructor(){
         super()
         this.state = {
-            topText: "",
-            bottomText: "",
-            imgUrl: "https://i.imgflip.com/345v97.jpg",
-            memeHolder: [],
-            number: 0
+            text1: "",
+            text2: "",
+            number: 0,
+            fontSize: "10px"
         }
     }
 
-    componentDidMount(){
-        fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(response => {
-            console.log(response.data.memes[0].url)
-            const {memes} = response.data
-            this.setState({
-                memeHolder: memes
-            })
-        })
-    }
-
-    
-
-    eventHandle = (event) => {
-        console.log("hello")
+    handleChange = event => {
+        console.log(event.target)
         const {name, value} = event.target
-        console.log([name])
-        this.setState({
-            [name]: value
-        })
-    }
-
-    submitHandle = (event) => {
-        event.preventDefault()
-        const eventName = event.target.name
-        
-        if (eventName === "next"){
-            this.setState({number: this.state.number + 1})
-        } else if (eventName === "prev") {
-            this.setState({number: this.state.number - 1})
-        }
-    
-        const randNum = Math.floor(Math.random() * this.state.memeHolder.length)
-        const randMeme = this.state.memeHolder[randNum].url
-        
-        eventName === "random" ? this.setState({imgUrl: randMeme, topText: "", bottomText: ""}) : this.setState({imgUrl: this.state.memeHolder[this.state.number].url, topText: "", bottomText: "" }) 
-        console.log(this.state.number)
-        
-
+        console.log(name, value)
+        this.setState({[name]: value}) 
     }
 
     render(){
-        const memeOption = this.state.memeHolder.map(list => {
-            return(
-                    <option key={list.id} value={list.url}>{list.name}</option>
-                
-            )
-        })
+        console.log(this.state.fontSize)
         return(
             <div>
-                <form className="meme-form">
+                <form className="textcontainer">
                     <input 
+                        placeholder="Enter Text"
                         type="text"
-                        placeholder="Top Text"
-                        value={this.state.topText}
-                        onChange={this.eventHandle}
-                        name="topText"
+                        onChange={this.handleChange}
+                        name="text1"
+                        value={this.state.text1}
                     />
                     <input 
+                        placeholder="Enter Text"
                         type="text"
-                        placeholder="Bottom Text"
-                        value={this.state.bottomText}
-                        onChange={this.eventHandle}
-                        name="bottomText"
+                        onChange={this.handleChange}
+                        name="text2"
+                        value={this.state.text2}        
                     />
-                    <button onClick={this.submitHandle} name="random">Random Meme</button>
-                    <button onClick={this.submitHandle} name="next">Next Meme</button>
-                    <button onClick={this.submitHandle} name="prev">Prevous Meme</button>
+                    <button onClick={this.handleClick} name="random" >Random Meme</button>
+                    <button onClick={this.handleClick} name="next" >Next Meme</button> 
+                    <button onClick={this.handleClick} name="prev" >Previous Meme</button> 
                     <label>
-                    Pick your favorite meme:
-                    <select onChange={this.eventHandle} name="imgUrl">
-                        {memeOption}
-                    </select>
+                        Font size
+                        <select placeholder="Font Size" name="fontSize" onChange={this.handleChange}>
+                            <option value="40px">40px</option>
+                            <option value="80px">80px</option>
+                            <option value="120px">120px</option>
+                            <option value="160px">160px</option>
+                        </select>
                     </label>
+                    
                 </form>
-                <div className="meme">
-                    <img src={this.state.imgUrl} alt="problem" />
-                    <Draggable>
-                        <h2 className="top">{this.state.topText}</h2>
-                    </Draggable>
-                    <Draggable>
-                        <h2 className="bottom">{this.state.bottomText}</h2>
-                    </Draggable>
-                </div>   
+                <div style={{fontSize: this.state.fontSize}} className="imgcontainer">
+                    {this.state.text1}
+                    {this.state.text2}
+                </div>
             </div>
         )
     }
-        
-    
 }
+
+
 
 export default MemeGenerator
