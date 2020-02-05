6ae019732cf947138011cf1bf2e42924779ef454
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-02-04 23:55:34 -0500

    added features and css

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-// import Draggable from 'react-draggable';
+import Draggable from 'react-draggable';
 
 class MemeGenerator extends Component {
     constructor(){
@@ -8,18 +8,56 @@ class MemeGenerator extends Component {
             text1: "",
             text2: "",
             number: 0,
-            fontSize: "10px"
+            imgUrl: "https://i.imgflip.com/30b1gx.jpg",
+            fontSize: "36px",
+            memeHolder: []
         }
     }
 
     handleChange = event => {
-        console.log(event.target)
         const {name, value} = event.target
         console.log(name, value)
         this.setState({[name]: value}) 
     }
 
+    handleClick = event => {
+        event.preventDefault()
+        const eventName = event.target.name
+
+        if(eventName === "next"){
+            this.setState({number: this.state.number+= 1})
+        } else if(eventName === "prev" && this.state.number > 0){
+            this.setState({number: this.state.number-= 1})
+        }
+        console.log(this.state.number)
+
+        const nextMeme = this.state.memeHolder[this.state.number].url
+        const randNum = Math.floor(Math.random() * this.state.memeHolder.length)
+        const randMeme = this.state.memeHolder[randNum].url
+
+        eventName === "random" ? this.setState({imgUrl: randMeme, text1: "", text2: ""}) : this.setState({imgUrl: nextMeme, text1: "", text2: ""})
+
+    }
+    
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.data.memes)
+            this.setState({
+                memeHolder: response.data.memes
+            })
+        })
+    }
+
     render(){
+        console.log(this.state.memeHolder[3])
+        let fontOption= []
+        for(var i = 0; i <= 100; i+= 4){
+            fontOption.push(i)
+        }
+        const selectNumber = fontOption.map(number => {return(<option key={number} value={number+"px"}>{number}px</option>)})
         console.log(this.state.fontSize)
         return(
             <div>
@@ -42,19 +80,25 @@ class MemeGenerator extends Component {
                     <button onClick={this.handleClick} name="next" >Next Meme</button> 
                     <button onClick={this.handleClick} name="prev" >Previous Meme</button> 
                     <label>
-                        Font size
-                        <select placeholder="Font Size" name="fontSize" onChange={this.handleChange}>
-                            <option value="40px">40px</option>
-                            <option value="80px">80px</option>
-                            <option value="120px">120px</option>
-                            <option value="160px">160px</option>
+                        <select className="select" placeholder="Font Size" name="fontSize" onChange={this.handleChange}>
+                            <option value="">--Choose a Font-Size--</option>
+                            {selectNumber}
                         </select>
                     </label>
                     
                 </form>
                 <div style={{fontSize: this.state.fontSize}} className="imgcontainer">
-                    {this.state.text1}
-                    {this.state.text2}
+                    <img src={this.state.imgUrl} alt="problem"></img>
+                    <Draggable>
+                        <h2>
+                            {this.state.text1}
+                        </h2>
+                    </Draggable>
+                    <Draggable>
+                        <h2 style={{top:"50%"}}>
+                            {this.state.text2}
+                        </h2>
+                    </Draggable>
                 </div>
             </div>
         )
